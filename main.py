1c728af5d7abd857bf596bad088c57d564eb4cbe
hayhon2 <152912714+hayhon2@users.noreply.github.com>
2025-11-17 04:38:34 +0100

    Update bot token and modify response messages

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-
 import telebot
 import os
 
-TOKEN = os.getenv("BOT_TOKEN", "PUT-YOUR-TOKEN-HERE")
+# ضع التوكن هنا
+TOKEN = "8143605867:AAHN3qthuwKoG_K5bm9h56KzzP-ePodedeA"
 bot = telebot.TeleBot(TOKEN)
+
+# ضع ال ID هنا
+ADMIN_ID = 6568396855
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    bot.reply_to(msg, "مرحباً! البوت يعمل بنجاح ⚡")
+    bot.reply_to(msg, "⚡ مرحباً! البوت يعمل بنجاح.\nأهلاً بك يا LION.")
 
 @bot.message_handler(func=lambda m: True)
 def echo(msg):
-    bot.reply_to(msg, "تم الاستلام: " + msg.text)
+    bot.reply_to(msg, "🦁 تم الاستلام: " + msg.text)
 
 bot.infinity_polling()
